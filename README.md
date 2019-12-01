3e084d5b9e999ecb5a4d7d21735b7abcca39754f
afolabisunday <afolabisunday31@yahoo.com>
2019-12-01 21:31:42 +0100

    Final Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TRANSLATOR
+
+
+## A simple Translator Using Google Python Api "googletrans"
+
+## This is just a simple desktp based translator, it can translate from ANY language to Any language. This is based on google free api provided for Python, link to download will be available below.
+
+ [Download Googletrans here](https://pypi.org/project/googletrans/)
+
+ ## SCREENSCHOTS
+
+ -- ![](001.png)
+ -- ![](002.png)
+ -- ![](003.png)
+ -- ![](004.png)
---
+++ translator.py
@@ -0,0 +1,165 @@
+from googletrans import Translator
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.uic import loadUiType
+
+test ,_ = loadUiType('translate.ui')
+lang = {'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu',
+    'fil': 'Filipino',
+    'he': 'Hebrew'}
+
+class Main(QMainWindow, test):
+    def __init__(cls):
+        QMainWindow.__init__(cls)
+        cls.setupUi(cls)
+        cls.show()
+        cls.handleBtn()
+        for keys, values in lang.items():
+            cls.comboBox.addItem(values)
+
+        cls.label.setText('Type or paste Text to convert here')
+
+    def handleBtn(cls):
+        cls.pushButton_2.clicked.connect(cls.functText)
+
+    def functText(cls):
+        try:
+            text2edit = cls.textEdit.toPlainText()
+            dest1 = cls.comboBox.currentText()
+            translator = Translator()
+            text = translator.translate(text2edit, dest=dest1, src='auto')
+            dLang = translator.detect([text2edit])
+            for lan in dLang:
+                data = lan.lang
+                if data in lang:
+                    data = lang.get(data, "")
+                    cls.textEdit_3.setPlainText(text.text)
+                    cls.label.setText(f'You are converting from {data} to {dest1}')
+        except:
+            cls.statusBar().showMessage('Please Choose a Desitatnation language')
+
+
+
+
+
+
+def Run():
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    Run()
+
+
+translator = Translator()
+langs = translator.detect(['한국어'])
+for lang in langs:
+    print(lang.lang)
